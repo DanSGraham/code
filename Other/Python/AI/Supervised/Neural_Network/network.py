27233fb69e4580fbc0002b97b9367b734604b40a
DanSGraham <dan.s.graham@gmail.com>
2015-06-28 21:46:47 -0400

    Work more on Neural Network

--- Other/Python/AI/Supervised/Neural_Network/network.py
+++ Other/Python/AI/Supervised/Neural_Network/network.py
@@ -1,5 +1,15 @@
 #A program to connect neurons in a network, adjust weights
+#I did most of my research from this great resource:
+#http://neuralnetworksanddeeplearning.com/chap1.html
 #import numpy
+
+#TODO: 1. Add matrix functionality to speed up considerably. 
+#			Current version is for learning purposes.
+#		2. Add recurrent feature.
+#		3. Add genetic weight adjustment.
+#		4. Add fuzzy logic
+#		5. Other kinds of NN
+
 from neuron import *
 
 class Network:
@@ -8,6 +18,7 @@ class Network:
 	def __init__(self, num_input_vals, num_poss_out, num_neurons_array):
 		
 		self.neural_network = []
+		self.learn_rate = 1.0
 		#num_poss_out should be the same as the last number in \
 		#num_neurons_array.
 		
@@ -23,18 +34,62 @@ class Network:
 					
 					new_neuron = Neuron(num_neurons_array[prev_layer])
 					for it_neuron in self.neural_network[prev_layer]:
-						it_neuron.connectNeuron(new_neuron)
+						it_neuron.connect_neuron(new_neuron)
 					
 				else:
-					new_neuron = Neuron(num_inputs)
+					new_neuron = Neuron(num_input_vals)
 					
 				temp_array.append(new_neuron)
 			prev_layer += 1
 				
 			self.neural_network.append(temp_array)
-			
+
+	def set_learn_rate(self, new_learn_rate):
+		self.learn_rate = new_learn_rate
 		
-test = Network(5, 5, [1,4,4,5])
+	def quad_calc_error(self, exp_val_vector, calc_val_vector):
+		#Sets the error variable of each neuron based on inputs and outputs.
+		#Currently for the quadratic cost function only.
+		for i in range((len(self.neural_network) - 1), 0, -1):
+			#Iterates through each layer in the network and assigns
+			#errors to the neurons.
+			
+			if i >= (len(self.neural_network) - 1): #Set error of output layer
+				for j in range(len(calc_value_vector)): #Both vectors must align.
+					error_in_neuron = (calc_val_vector - exp_val_vector) \
+					* self.neural_network[i][j].d_activation_function(self.input_sum)
+					self.neural_network[i][j].set_error(error_in_neuron)
+			else:
+				for k in range(self.neural_network[i]): #Iterate through other layers setting errors.
+					#not sure how to calculate the error in other layers.
+					error_in_neuron = 
+					
+		
+	def quadratic_cost_function(self, exp_value_matrix, calc_val_matrix):
+		#Both params are 2d matrices with the values in columns equal to
+		#output from a certain set of inputs. The number of columns is 
+		#equal to the number of values trained on.
+		out_vector = []
+		cost_vector = []
+		#The total cost vector at the end of the calculation.
+		for i in range(calc_val_martix):
+			for j in range(calc_val_matrix[i]):
+				vector_val = (exp_value_matrix[i][j] - \
+								calc_val_matrix[i][j])**2
+				if i == 0:
+					out_vector.append(vector_val)
+				else:
+					out_vector[j] += vector_val
+				
+		for unaveraged_val in out_vector:
+			averaged_val = unaveraged_val / (2,0 * len(calc_val_matrix))
+			cost_vector.append(averaged_val)
+		
+		return cost_vector
+		
+	
+				
+			
 			
 				
 				
--- Other/Python/AI/Supervised/Neural_Network/neuron.py
+++ Other/Python/AI/Supervised/Neural_Network/neuron.py
@@ -7,61 +7,84 @@ import math
 	
 class Neuron:
 	#A class to specify neurons in a neural network. 
-	def __init__(self, numberOfInputs):
-		#NEED Weights of all inputs. Will the output or input node store weights? (thinking output)
-		#Need activation function (sigmoid function or step)
-		#NEed Bias!
+	def __init__(self, number_inputs):
 		self.bias = 1
-		self.downStreamNeurons = []
-		self.sigSlopeParam = 1
-		self.activationFunction = self.LogSigmoid
+		self.output_value = 0
+		self.input_sum = 0
+		self.downstream_neurons = []
+		self.activation_function = None
+		self.error = 0
+		self.set_activation_function_sigmoid()
 		
 		#create weight values. Between -1 and 1 initially. Weights match
 		#indeces of inputs.
 		self.weights = []	
-		for i in range(numberOfInputs):
+		for i in range(number_inputs):
 			self.weights.append(random.uniform(-1, 1))
 			
 		
-		#add weight for bias. Always at the end of the array.	
-		self.weights.append(random.uniform(-1, 1))
+		#add weight for bias. Always at the end of the array. 
+		#May instead have a bias that changes rather than weight.
+		#Will do a few tests to determine best scenario.	
+		#self.weights.append(random.uniform(-1, 1))
 		
-		#Possibly don't need weight for bias. Some examples have direct 
-		#Change of bias.
+	def set_bias(self, new_bias):
+		#This method may not be necessary if it can be done with weights
+		self.bias = new_bias
+		
+	def set_error(self, new_error):
+		self.error = new_error
 	
-	def setActivateFunctionStep(self):
+	def set_activation_function_step(self):
 		#Sets the activation function to a step function
 		pass
 		
-	def setActivationFunctionSigmoid(self):
+	def set_activation_function_sigmoid(self, sig_slope_param = 1):
 		#Sets activation function as a sigmoid. Possibly adjust the parameters of the sigmoid if necessary
-		self.sigSlopeParam = 1
+		self.sig_slope_param = sig_slope_param
+		self.activation_function = self.log_sigmoid
+		self.d_activation_function = self.d_log_sigmoid
 	
-	def setSigSlopeParam(self, newParam):
-		self.sigSlopeParam = newParam
+	def set_sig_slope_param(self, new_param):
+		self.sig_slope_param = new_param
 			
-	def processInput(self, inputArray):
-		#Processes the input and determines if it will fire.
-		pass
+	def process_input(self, input_array):
+		#Processes the input and determines if it will fire. 
+		#Returns the value of the activation_function
+		#Also stores the value of the output temporarily for use 
+		# in self.output.
+		sum_inputs = sum(input_array)
+		self.input_sum = sum_inputs
+		activation_val = self.activation_function(sum_inputs)
+		self.output_value = activation_value
+		return activation_val
 		
-	def adjustWeights(self, weightAdjust):
+	def adjust_weights(self, weight_adjust):
 		#Adjusts the weights of the inputs
 		pass
 		
-	def connectNeuron(self, neuronToConnect):
-		self.downStreamNeurons.append(neuronToConnect)
+	def connect_neuron(self, neuron_to_connect):
+		self.downstream_neurons.append(neuron_to_connect)
 	
-	def LogSigmoid(sumOfInputs):
+	def log_sigmoid(sum_inputs):
 		#returns a value from 0 to 1
-		sigValue = 	1.0/(1.0 + (math.e ** (-1 * self.sigSlopeParam * sumOfInputs)))
-		return sigValue
+		sig_val = 	1.0/(1.0 + (math.e ** (-1 * self.sig_slope_param * sum_inputs)))
+		return sig_val
 		
-	def TanHyperbolic(sumOfInputs):
+	def d_log_sigmoid(sum_inputs):
+		#Returns the derivative of the log_sigmoid activation function.
+		
+		sig_derivative = (math.e ** sum_inputs) \
+		/ ((1 + math.e ** sum_inputs) ** 2)
+		
+		return sig_derivative
+		
+	def tan_hyperbolic(sum_inputs):
 		#returns a value from -1 to 1
-		hypValue = math.tanh(sumOfInputs)
-		return hypValue
+		hyp_value = math.tanh(sum_inputs)
+		return hyp_value
 		
-	def SoftMaxFunction(sumOfInputs):
+	def soft_max(sumOfInputs):
 		#Need to look up the softMaxFunction
 		#Starting to get it. Involves a probability of outputs. 
 		#Need more research. Lack fundamental understanding.
@@ -74,15 +97,14 @@ class Neuron:
 		#each neuron and divides by the sum of all neurons. Thus the
 		#probability of the neuron's output being correct is given. The
 		#output neurons are numbered to correspond to the poss. outputs.
+		#Soft max must have access to all other neurons in the layer.
 		pass
 		
 		
-	def StepFunction(sumOfInputs):
+	def step_function(sum_inputs):
 		#returns either -1 or 1
-		if (sumOfInputs >= 0):
+		if (sum_inputs >= 0):
 			return 1
 		else:
 			return 0
-	
-		
-	
+			
