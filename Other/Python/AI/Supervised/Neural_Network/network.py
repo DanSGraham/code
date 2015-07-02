2281ee7ab62a1a18ec01aad3c22f0f0493fbd950
DanSGraham <dan.s.graham@gmail.com>
2015-07-02 18:49:30 -0400

    Comment and update neural network

--- Other/Python/AI/Supervised/Neural_Network/network.py
+++ Other/Python/AI/Supervised/Neural_Network/network.py
@@ -1,9 +1,50 @@
-#A program to connect neurons in a network, adjust weights
-#I did most of my research from this great resource:
-#http://neuralnetworksanddeeplearning.com/chap1.html
+"""This is a neural network: the organizational structure on which 
+neurons are arrayed. The network not only stores the order of the 
+neurons, but it is also responsible for calculating the cost and errors
+associated with the whole network, and report those errors back to 
+individual neurons for weight adjustments. The neurons can be organized
+in any fashion in a network, but the most fundamental kind of network is
+a feed forward network. This kind of network has 3 basic 'layers' of
+neurons (In one layer of neurons the neurons of that layer are not 
+connected to each other in anyway, but all connect to the neurons in the
+adjacent forward layer):
+
+1. Input layer: The input layer is where the inputs for the neural 
+	network are received. This layer takes raw data from your data set 
+	and sends it to the next layer.
+	
+2. Hidden layer: The hidden layer (or layers) are those that are not 
+	input layers, but are not output layers either (output layers will
+	be explained shortly). Hidden layers allow further processing and
+	adjustments to be made to the data before it is sent to the output 
+	layer. The more complex the hidden layers, the more complex of an
+	equation the system can model.
+	
+3. Output layer: This is the layer that returns a value. Each neuron in
+	this layer corresponds to a binary type value. If you want to
+	predict true or false (binary possibility) given any inputs, you
+	can use 1 output neuron, because the neuron can output 2 possible 
+	values effectively. If you want to predict a number between 1 and 10
+	you would need 10 output neurons and assign each a number in the 
+	desired range. Then you can determine which is the output by the 
+	largest output value.
+
+Neurons in a network have the capacity to model incredibly complex 
+equations, and usually the more neurons in hidden layers, the more 
+complex the model can become. The trade off, as usual, is time and 
+occasional overfitting. The network allows neurons to communicate with
+each other and send errors through the network, allowing the whole 
+network to improve over time.
+
+The main resource I used for this project may be found here:
+	http://neuralnetworksanddeeplearning.com/chap1.html"""
+#By Daniel Graham
+
 #import numpy
 
-#TODO: 1. Add matrix functionality to speed up considerably. 
+#TODO: 
+#		0. Check over code. Do all the functions act correctly? Are they named appropriately? Commented so anyone can pick it up and understand how it works?
+#		1. Add matrix functionality to speed up considerably. 
 #			Current version is for learning purposes.
 #		2. Add recurrent feature.
 #		3. Add genetic weight adjustment.
@@ -11,9 +52,10 @@
 #		5. Other kinds of NN
 
 from neuron import *
+import math
 
 class Network:
-	
+	#A class to specify a network of neurons
 	
 	def __init__(self, num_input_vals, num_poss_out, num_neurons_array):
 		
@@ -56,13 +98,21 @@ class Network:
 			
 			if i >= (len(self.neural_network) - 1): #Set error of output layer
 				for j in range(len(calc_value_vector)): #Both vectors must align.
-					error_in_neuron = (calc_val_vector - exp_val_vector) \
-					* self.neural_network[i][j].d_activation_function(self.input_sum)
+					error_in_neuron = (calc_val_vector[j] - exp_val_vector[j]) \
+					* self.neural_network[i][j].d_activation_function(self.neural_network[i][j].input_sum)
 					self.neural_network[i][j].set_error(error_in_neuron)
 			else:
 				for k in range(self.neural_network[i]): #Iterate through other layers setting errors.
 					#not sure how to calculate the error in other layers.
-					error_in_neuron = 
+					error_in_neuron = 0
+					curr_neuron = self.neural_network[i][k]
+					for down_neuron in curr_neuron.downstream_neurons:
+						error_in_neuron += down_neuron.weights[k] * \
+											down_neuron.error
+					error_in_neuron *= curr_neuron.d_activation_function(curr_neuron.input_sum)
+					curr_neuron.error = error_in_neuron
+
+					
 					
 		
 	def quadratic_cost_function(self, exp_value_matrix, calc_val_matrix):
@@ -87,7 +137,6 @@ class Network:
 		
 		return cost_vector
 		
-	
 				
 			
 			
--- Other/Python/AI/Supervised/Neural_Network/neuron.py
+++ Other/Python/AI/Supervised/Neural_Network/neuron.py
@@ -1,29 +1,63 @@
+"""This is a neuron: the base component of a neural network. It is meant
+to function similar to a biological neuron, accepting inputs and based
+on how large the input signal is fire an output signal. The neuron has
+3 main components:
+1. Weights for incoming stimuli (outputs from connected neurons): These 
+	weights are what the neuron uses to set its "fire" threshold. They 
+	are adjustable and allow the neuron, in concert with the network, to 
+	adapt. 
+	
+2. Activation function: This is the function that incoming stimulus
+	(the sum of all inputs multiplied by weights) are evaluated by. The
+	activation funcition converts the incoming stimulus into a standard
+	form, for a step funciton 0 or 1, and for a sigmoid function a value
+	between 0 and 1 along a sigmoid curve.
+	
+3. Bias: This allows the neuron to fire even if no stimulus is recieved
+	(if all values are 0) this acts essentially like an internal 
+	stimulus to the neuron, allowing it to fire on a 0 input, which may
+	be useful to the network as a whole. The bias is adjusted like a 
+	weight.
+	
+These things together make up a neuron. They can be specified in a 
+matrix or neuron objects. When connected in multiple layers, the neurons
+may act like an equation where the weights are the coefficients. In this
+way a neural network can approximate many types of equations by 
+adjusting weights.
+
+The main resource I used for this project may be found here:
+	http://neuralnetworksanddeeplearning.com/chap1.html"""
+#By Daniel Graham
+
+
+
 import random
 import math
-
-#TO solve the problem of output weighting have a special output neuron with only one input use to determine weight of final output if needed. May find that such a neuron is not needed.
-
 
 	
 class Neuron:
 	#A class to specify neurons in a neural network. 
 	def __init__(self, number_inputs):
 		self.bias = 1
-		self.output_value = 0
-		self.input_sum = 0
-		self.downstream_neurons = []
-		self.activation_function = None
-		self.error = 0
+		self.output_value = 0 #Output from activation function
+		self.input_stimulus = 0  #Sum of inputs multiplied by weights
+		self.downstream_neurons = [] 
+		self.activation_function = None 
+		self.error = 0 
 		self.set_activation_function_sigmoid()
 		
-		#create weight values. Between -1 and 1 initially. Weights match
-		#indeces of inputs.
+		#MAY NEED UNWEIGHTED INPUTS AS WELL. MAKE SURE NOT NECESSARY.
+		
+		#Activation is initially set to sigmoid because this is the 
+		#most common activation function in basic networks.
+		
+		
+		#Create weight values. Between -1 and 1 initially. Weight index 
+		#in array corresponds to index of input to weight.
 		self.weights = []	
 		for i in range(number_inputs):
 			self.weights.append(random.uniform(-1, 1))
 			
-		
-		#add weight for bias. Always at the end of the array. 
 		#May instead have a bias that changes rather than weight.
 		#Will do a few tests to determine best scenario.	
 		#self.weights.append(random.uniform(-1, 1))
@@ -33,19 +67,35 @@ class Neuron:
 		self.bias = new_bias
 		
 	def set_error(self, new_error):
+		#The error in a neuron is what motivates the adjustment of 
+		#weights, each adjustment is meant to reduce neuron self.error. 
 		self.error = new_error
 	
 	def set_activation_function_step(self):
-		#Sets the activation function to a step function
-		pass
+		#Sets the activation function to a step function.
+		#This is not really a useful activation function because it 
+		#causes drastic swings in the network when a weight changes. 
+		#Drastic swings are caused by the binary values of a step fxn.
+		
+		self.activation_function = step_function
+		self.d_activation_function = 0 
 		
 	def set_activation_function_sigmoid(self, sig_slope_param = 1):
-		#Sets activation function as a sigmoid. Possibly adjust the parameters of the sigmoid if necessary
+		#Sets activation function as a sigmoid. Can adjust the 
+		#parameters of the sigmoid if necessary. This is a useful 
+		#activation function because there is a 'soft' change from 0
+		#to 1 when compared to the step function, resulting in much more
+		#gradual output changes.
+		
 		self.sig_slope_param = sig_slope_param
 		self.activation_function = self.log_sigmoid
 		self.d_activation_function = self.d_log_sigmoid
 	
 	def set_sig_slope_param(self, new_param):
+		#The slope parameter determines how 'soft' or gradual the change
+		#from 0 to 1 outputs are in the function. A smaller parameter
+		#means a smaller slope.
+		
 		self.sig_slope_param = new_param
 			
 	def process_input(self, input_array):
@@ -53,9 +103,22 @@ class Neuron:
 		#Returns the value of the activation_function
 		#Also stores the value of the output temporarily for use 
 		# in self.output.
-		sum_inputs = sum(input_array)
-		self.input_sum = sum_inputs
-		activation_val = self.activation_function(sum_inputs)
+		
+		#Each input is multiplied by its corresponding weight and then
+		#all inputs are summed and stored in the neuron memory 
+		#(self.input_sum).
+		
+		weighted_inputs = []
+		for i in range(input_array):
+			weighted_inputs.append(input_array[i] * self.weights[i])
+		
+		self.input_stimulus = sum(weighted_inputs) + self.bias
+			
+		#Then the input stimulus is sent to the activation fxn which 
+		#generates the output of the neuron, also stored in the neuron
+		#memory (self.output_value).
+		
+		activation_val = self.activation_function(self.input_stimulus)
 		self.output_value = activation_value
 		return activation_val
 		
@@ -64,32 +127,34 @@ class Neuron:
 		pass
 		
 	def connect_neuron(self, neuron_to_connect):
+		#Connects the current neuron to the next layer. The neurons in 
+		#self.downstream_neurons receive the output of the self neuron.
+		
 		self.downstream_neurons.append(neuron_to_connect)
 	
-	def log_sigmoid(sum_inputs):
-		#returns a value from 0 to 1
-		sig_val = 	1.0/(1.0 + (math.e ** (-1 * self.sig_slope_param * sum_inputs)))
+	def log_sigmoid(input_stimulus):
+		#Returns a value from 0 to 1 and has a variable slope.
+		
+		sig_val = 	1.0/(1.0 + (math.e ** (-1 * self.sig_slope_param * \
+											input_stimulus)))
 		return sig_val
 		
-	def d_log_sigmoid(sum_inputs):
+	def d_log_sigmoid(input_stimulus):
 		#Returns the derivative of the log_sigmoid activation function.
+		#The derivative is used when determing the error in a neuron.
 		
-		sig_derivative = (math.e ** sum_inputs) \
-		/ ((1 + math.e ** sum_inputs) ** 2)
+		sig_derivative = (math.e ** input_stimulus) \
+							/ ((1 + math.e ** input_stimulus) ** 2)
 		
 		return sig_derivative
 		
-	def tan_hyperbolic(sum_inputs):
-		#returns a value from -1 to 1
-		hyp_value = math.tanh(sum_inputs)
+	def tan_hyperbolic(input_stimulus):
+		#Returns a value from -1 to 1. Similar to log_sigmoid but 
+		#offers negative values.
+		hyp_value = math.tanh(input_stimulus)
 		return hyp_value
 		
-	def soft_max(sumOfInputs):
-		#Need to look up the softMaxFunction
-		#Starting to get it. Involves a probability of outputs. 
-		#Need more research. Lack fundamental understanding.
-		
-		
+	def soft_max(input_stimulus):
 		#SoftMaxFunction is slightly different from the others.
 		#It requires an output neuron for each possible output value
 		#(outputs are not binary, 1 or 0, they take on multiple values)
@@ -101,9 +166,10 @@ class Neuron:
 		pass
 		
 		
-	def step_function(sum_inputs):
-		#returns either -1 or 1
-		if (sum_inputs >= 0):
+	def step_function(input_stimulus):
+		#Returns either -1 or 1 no values between. Not very useful for
+		#neurons.
+		if (input_stimulus >= 0):
 			return 1
 		else:
 			return 0
