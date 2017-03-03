4b8dabdf473f22b98f0a686e60f012ad66150d28
DanSGraham <dan.s.graham@gmail.com>
2017-03-03 01:24:59 -0500

    Finish Stochastic Train and begin cleanup

--- Other/Python/AI/Supervised/Neural_Network/brain.py
+++ Other/Python/AI/Supervised/Neural_Network/brain.py
@@ -12,6 +12,10 @@ from pybrain.structure import FeedForwardNetwork, SigmoidLayer, FullConnection
 
 
 #TODO: 
+#Docstring
+Clean up
+#Currently going with a 70/30 train to test method.
+#Do ROC analysis for error
 #Add debug/evaluation methods to test effectiveness of different methods
 #print out graph of improvement over time or error over time.
 #Code various training regimes
@@ -24,19 +28,20 @@ class Brain:
         """Initializes the brain"""
         self.network = neural_network.create_network(networkInputFile)
         
-        self.tVerbose = False
+        self.maxEpochs = None
         if networkInputFile["trainingMethod"] == "Batch":
             self.tFactor = networkInputFile["trainingProperties"]["trainingFactor"]
             self.bSize = networkInputFile["trainingProperties"]["batchSize"]
-            if "verbose" in networkInputFile["trainingProperties"]:
-                self.tVerbose = networkInputFile["trainingProperties"]["verbose"]
             self.training_method = batch_train 
         if networkInputFile["trainingMethod"] == "Online":
             self.tFactor = networkInputFile["trainingProperties"]["trainingFactor"]
-            if "verbose" in networkInputFile["trainingProperties"]:
-                self.tVerbose = networkInputFile["trainingProperties"]["verbose"]
             self.bSize = 1
             self.training_method = online_train
+        if networkInputFile["trainingMethod"] == "Stochastic":
+            self.tFactor = networkInputFile["trainingProperties"]["trainingFactor"]
+            self.bSize = networkInputFile["trainingProperties"]["batchSize"]
+            self.maxEpochs = networkInputFile["trainingProperties"]["maxEpochs"]
+            self.training_method = stochastic_train
 
         if "trainingSet" in networkInputFile:
             self.trainingSet = networkInputFile["trainingSet"]
@@ -62,7 +67,10 @@ class Brain:
             output_vals[i] = np.array(output_vals[i])
         
         #Run training method
-        return self.training_method(self.network, input_vals, output_vals, self.bSize, self.tFactor, self.tVerbose)
+        if self.maxEpochs != None:
+            return self.training_method(self.network, input_vals, output_vals, self.maxEpochs, self.bSize, self.tFactor)
+        else:
+            return self.training_method(self.network, input_vals, output_vals, self.bSize, self.tFactor)
 
     def predict(self, inputSet):
         """Predicts an output based on the trained values"""
@@ -73,17 +81,14 @@ class Brain:
 
 ##----------------------Training Methods---------------------------------------
 
-def batch_train(network, input_set, output_set, batch_size, train_factor, verbose=False):
+def batch_train(network, input_set, output_set, batch_size, train_factor):
 
     leftover_data = len(input_set) % batch_size
     upper_limit = len(input_set) - leftover_data
 
-    training_error = 0.0
-
     for i in range(0, upper_limit, batch_size):
         weights_error = []
         bias_error = []
-        batch_error = 0.0
 
         for n in range(len(network.network_weights)):
             weights_error.append(np.zeros(network.network_weights[n].shape))
@@ -91,72 +96,67 @@ def batch_train(network, input_set, output_set, batch_size, train_factor, verbos
 
         for j in range(batch_size):
             network_output = network.calculate(input_set[i + j])
-            w_err_tmp, b_err_tmp = network.correction(output_set[i + j])
+            w_err_tmp, b_err_tmp, MSE = network.correction(output_set[i + j])
 
             for n in range(len(weights_error)):
                 weights_error[n] += w_err_tmp[n]
                 bias_error[n] += b_err_tmp[n]
 
-            batch_error += np.divide(np.sum(np.multiply(np.divide(np.abs(\
-                               output_set[i + j] - network_output), \
-                               output_set[i + j]), 100.0)), len(network_output))
-
-        batch_error = batch_error / float(batch_size)
-        if verbose:
-            print batch_error, "% batch train error"
-        
-        training_error += batch_error
-
         for n in range(len(weights_error)):
             weights_error[n] = np.multiply((float(train_factor) / batch_size), weights_error[n])
             bias_error[n] = np.multiply((float(train_factor) / batch_size), bias_error[n])
-        
         network.adjustment(weights_error, bias_error)
 
-    
+     
     weights_error = []
     bias_error = []
-    batch_error = 0.0
     for n in range(len(network.network_weights)):
         weights_error.append(np.zeros(network.network_weights[n].shape))
         bias_error.append(np.zeros(network.network_bias[n].shape))
 
     for k in range(leftover_data):
         network_output = network.calculate(input_set[upper_limit + k])
-        w_err_tmp, b_err_tmp = network.correction(output_set[upper_limit + k])
+        w_err_tmp, b_err_tmp, MSE = network.correction(output_set[upper_limit + k])
         for n in range(len(weights_error)):
             weights_error[n] += w_err_tmp[n]
             bias_error[n] += b_err_tmp[n]
         
-        batch_error += np.divide(np.sum(np.multiply(np.divide(np.abs(\
-                           output_set[upper_limit + k] - network_output), \
-                           output_set[upper_limit + k]), 100.0)), len(network_output))
-
-        batch_error = batch_error / float(batch_size)
-        if verbose:
-            print batch_error, "% batch train error"
-            #Possibly return the value in list rather than print
-        
-        training_error += batch_error
-
         for n in range(len(weights_error)):
             weights_error[n] = np.multiply((float(train_factor) / batch_size), weights_error[n])
             bias_error[n] = np.multiply((float(train_factor) / batch_size), bias_error[n])
 
     network.adjustment(weights_error, bias_error)
+    return True
 
+def online_train(network, input_set, output_set, batch_size, train_factor):
+    return batch_train(network, input_set, output_set, 1, train_factor)
+
+def single_epoch(network, input_matrix, output_matrix, minibatch_size, train_factor, train_to_test_ratio = 0.7):
+    #Trains a single epoch.
+    in_copy = []
+    out_copy = []
+    index_list = range(len(input_matrix))
+    random.shuffle(index_list)
+    for i in range(int(len(input_matrix) * train_to_test_ratio)):
+        in_copy.append(np.copy(input_matrix[index_list[i]]))
+        out_copy.append(np.copy(output_matrix[index_list[i]]))
+    batch_train(network, in_copy, out_copy, minibatch_size, train_factor)
+    #Determine error in test set. 
+    tot_SSE = 0.0
+    for j in range(int(len(input_matrix) * train_to_test_ratio), len(input_matrix)):
+        network.calculate(input_matrix[index_list[j]])
+        weight_err, bais_err, MSE = network.correction(output_matrix[index_list[j]])
+        tot_SSE += MSE
+
+    return tot_SSE
     
-    training_error = training_error / ((float(upper_limit) / batch_size) + int(bool(leftover_data)))
-    return training_error
-
-def online_train(network, input_set, output_set, batch_size, train_factor, verbose=False):
-    return batch_train(network, input_set, output_set, 1, train_factor, verbose)
-
-def stochastic_train(network, input_set, output_set, train_size, test_size, max_num_epochs, report_epochs, max_error_val, train_factor, verbose=False):
-    pass
-
-
-
+def stochastic_train(network, input_set, output_set, max_num_epochs, batch_size, train_factor):
+   
+    SSE_list = []
+    for i in range(max_num_epochs):
+        SSE_list.append(single_epoch(network, input_set, output_set, batch_size, train_factor))
+    return SSE_list
+    
 #Brain has attributes, network, braintype.
 
 #Depending on brain type brain can have limited resources.
--- Other/Python/AI/Supervised/Neural_Network/neural_network.py
+++ Other/Python/AI/Supervised/Neural_Network/neural_network.py
@@ -2,19 +2,24 @@
 # By DanG
 
 #TODO:
+#Use Abstract Base Classes.
+#Write DocStrings
+#Clean up code
 #Optimize MLPercpetron
-#Code other correction algorithms
 #Code other Network Types
 #Implement network save funcitonality
-
 
 import json
 import numpy as np
 import time
 
+
 class NeuralNetwork(object):
 
-    def __init__(self, input_file):
+
+
+    def __init__(self, input_dict):
+
         self.activation_function = log_sigmoid 
         self.d_activation_function = d_log_sigmoid
         self.slope_param = 1.0
@@ -23,7 +28,6 @@ class NeuralNetwork(object):
         self.use_bias = True
 
         self.properties = input_file["networkProperties"]
-
         if "activationFunction" in self.properties:
             if self.properties["activationFunction"] == "tanh":
                 self.activation_function = tan_hyperbolic
@@ -35,79 +39,112 @@ class NeuralNetwork(object):
             self.use_bias = self.properties["useBias"]
 
         if "costFunction" in self.properties:
-            self.cost_function = self.properties["costFunction"]
-
+            cFun = self.properties["costFunction"]
+            if cFun == "Quadratic":
+                self.cost_function = quad_cost_grad
+            if cFun == "CrossEntropy":
+                self.cost_function = cross_entropy_cost_grad
         if "optimizationMethod" in self.properties:
             self.optimization = self.properties["optimizationMethod"]
 
     def calculate(self, input_data):
-        raise NotImplementedError("You have not implemented the calculate method")
+
+        raise NotImplementedError(
+            "You have not implemented the calculate method")
 
     def correction(self):
-        raise NotImplementedError("You have not implemented the correction method")
+
+        raise NotImplementedError(
+            "You have not implemented the correction method")
 
     def adjustment(self):
-        raise NotImplementedError("You have not implemented the adjustment method")
+
+        raise NotImplementedError(
+            "You have not implemented the adjustment method")
 
     def export_network(self):
-        raise NotImplementedError("You have not implemented the export_network method")
+
+        raise NotImplementedError(
+            "You have not implemented the export_network method")
 
 
-class FNeuralNetwork(NeuralNetwork):
+class FFNeuralNetwork(NeuralNetwork):
 
-    def __init__(self, input_data):
-        super(FNeuralNetwork, self).__init__(input_data)
-        self.input_data = input_data
+
+
+    def __init__(self, input_dict):
+
+        super(FFNeuralNetwork, self).__init__(input_dict)
+        self.input_data = input_dict
         self.num_inputs = self.properties['numberInputs']
+        self.num_outputs = self.properties["numberOutputs"]
 
     def calculate(self, input_data):
+
         pass
 
     def correction(self):
+
         pass
 
     def adjustment(self):
+
         pass
 
     def export_network(self):
+
         return json.JSONEncoder(self.input_data)
 
-class SingleLayerPerceptron(FNeuralNetwork):
 
-    def __init__(self, input_data):
-        super(SingleLayerPerceptron, self).__init__(input_data)
-        self.num_neurons = self.properties["numberOutputs"]
-        self.network_weights = np.random.uniform(-1, 1, (self.num_neurons, self.num_inputs))
-        self.bias_weights = np.random.uniform(-1, 1, (self.num_neurons))
-        if not self.use_bias:
-            self.bias_weights = np.zeros(self.num_neurons)
+class SingleLayerPerceptron(FFNeuralNetwork):
 
-    #Correct calculate and correction for new input status.
+
+
+    def __init__(self, input_dict):
+
+        super(SingleLayerPerceptron, self).__init__(input_dict)
+        self.num_neurons = self.num_outputs
+        self.network_weights = np.random.uniform(-1, 1, 
+            (self.num_neurons, self.num_inputs))
+        self.bias_weights = np.random.uniform(-1, 1, 
+            (self.num_neurons))
+
     def calculate(self, input_stimulus):
+
         #input_stiumulus is a 1D array in bra form
-        self.last_input = input_stimulus
-        self.last_output = np.multiply(self.slope_param, self.network_weights.dot(input_stimulus)) + self.bias_weights
-        self.activated_values = self.activation_function(self.last_output)
+        self.data_layers = [input_stimulus]
+        self.activation_input = [
+            np.multiply(self.slope_param, 
+            self.network_weights.dot(input_stimulus))
+            + self.bias_weights
+        ]
+        self.data_layers.append(self.activation_function(self.last_output))
         return self.activated_values
 
-    def correction(self, true_values):
-        #Returns the error matrix from the last run.
-        if self.optimization == "backpropagation":
-           self.error = np.multiply(self.cost_function(self.activated_values, true_values), self.d_activation_function(self.last_output, self.slope_param))
+    def correction(self, target_values):
 
-        return (np.multiply(self.error, self.last_input), self.error)
+        deriv_act_fun = self.d_activation_function(
+            self.activation_input[-1], self.slope_param)
+        self.error = np.multiply(self.cost_function(self.data_layers[-1], 
+            target_values), deriv_act_fun)
+        return self.error
 
     def adjustment(self, weights_adjust, bias_adjust):
+
         self.network_weights = self.network_weights - weights_adjust
         self.bias_weights = self.bias_weights - bias_adjust
 
     def export_network(self):
+        
         return json.JSONEncoder(self.input_data)
 
 
-class MultiLayerPerceptron(FNeuralNetwork):
+class MultiLayerPerceptron(FFNeuralNetwork):
+
+
 
     def __init__(self, input_data):
+
         super(MultiLayerPerceptron, self).__init__(input_data)
         self.size_init_layer = self.properties["sizeInitialLayer"]
         self.num_hidden_layers = self.properties["numberHiddenLayers"]
@@ -118,162 +155,227 @@ class MultiLayerPerceptron(FNeuralNetwork):
         self.network_bias = []
 
         if self.normalize_input:
-            self.network_weights.append(np.random.uniform(-1, 1, (self.num_inputs)))
-            self.network_bias.append(np.random.uniform(-1, 1, (self.num_inputs)))
+            self.network_weights.append(
+                np.random.uniform(-1, 1, (self.num_inputs)))
+            self.network_bias.append(
+                np.random.uniform(-1, 1, (self.num_inputs)))
 
         #Create Input Layer:
-        self.network_weights.append(np.random.uniform(-1, 1, (self.size_init_layer, self.num_inputs)))
-        self.network_bias.append(np.random.uniform(-1, 1, (self.size_init_layer)))
+        self.network_weights.append(
+            np.random.uniform(-1, 1, (self.size_init_layer, self.num_inputs)))
+        self.network_bias.append(
+            np.random.uniform(-1, 1, (self.size_init_layer)))
 
         temp_inputs = self.size_init_layer
         for i in range(self.num_hidden_layers):
             layer_size = self.hidden_layer_sizes[i]
-            self.network_weights.append(np.random.uniform(-1, 1, (layer_size, temp_inputs)))
-            self.network_bias.append(np.random.uniform(-1, 1, (layer_size)))
+            self.network_weights.append(
+                np.random.uniform(-1, 1, (layer_size, temp_inputs)))
+            self.network_bias.append(
+                np.random.uniform(-1, 1, (layer_size)))
             temp_inputs = layer_size
 
         #Create output layer:
-        self.network_weights.append(np.random.uniform(-1, 1, (self.num_output_neurons, temp_inputs)))
-        self.network_bias.append(np.random.uniform(-1, 1, (self.num_output_neurons)))
+        self.network_weights.append(
+            np.random.uniform(-1, 1, (self.num_output_neurons, temp_inputs)))
+        self.network_bias.append(
+            np.random.uniform(-1, 1, (self.num_output_neurons)))
 
         if not self.use_bias:
             for bias_layer in self.network_bias:
                 bias_layer.fill(0.0)
 
     def calculate(self, input_values):
-        #input stimulus is a 1D array in bra form 
-        #This will store the output of each layer which is the input of the next.
-        #Check this operaiton for accuracy.
 
+        #input stimulus is a 1D array in bra form 
         self.data_layers = [input_values]
         self.activation_input = []
 
         iter_val = range(len(self.network_weights))
         if self.normalize_input:
-            self.activation_input.append(np.multiply(self.slope_param, np.multiply(self.data_layers[-1], \
+            self.activation_input.append(
+                np.multiply(self.slope_param, np.multiply(self.data_layers[-1],
                 self.network_weights[0]) + self.network_bias[0]))
-            self.data_layers.append(self.activation_function(self.activation_input[-1]))
+            self.data_layers.append(
+                self.activation_function(self.activation_input[-1]))
             iter_val = range(1,len(self.network_weights)) 
 
         for i in iter_val:
-            self.activation_input.append(np.multiply(self.slope_param, np.dot(self.network_weights[i], self.data_layers[-1])\
-                                         + self.network_bias[i]))
-            self.data_layers.append(self.activation_function(self.activation_input[-1]))
+            self.activation_input.append(
+                np.multiply(self.slope_param, 
+                np.dot(self.network_weights[i], self.data_layers[-1])
+                + self.network_bias[i]))
+            self.data_layers.append(
+                self.activation_function(self.activation_input[-1]))
         
         return self.data_layers[-1] 
 
-    def correction(self, true_values):
+    def correction(self, target_values):
+        
         #Generate the error matrices to make adjustments.
-        #backpropagation correction algorithm.
-        
-        
         #Iterate backwards through layers calculating error.
         self.error_list = []    #This list must be flipped around at the end.
         self.weights_error = [] #This must also be flipped
         #OutputError first
-        self.error_list.append(np.multiply(self.cost_function(self.data_layers[-1], true_values), \
-                               self.d_activation_function(self.activation_input[-1], self.slope_param)))
+        self.error_list.append(
+            np.multiply(
+                self.cost_function(self.data_layers[-1], target_values),
+                self.d_activation_function(
+                    self.activation_input[-1], self.slope_param)))
         
-        self.weights_error.append(np.dot(np.array([self.error_list[-1]]).T, np.array([self.data_layers[-2]])))
+       #Need to calculate the weight error outside in the train function 
+        self.weights_error.append(
+            np.multiply(np.array([self.error_list[-1]]).T, 
+            np.array([self.data_layers[-2]])))
 
         for i in range((len(self.network_weights)-2), -1, -1):
-            self.error_list.append(np.multiply(np.dot(self.network_weights[i + 1].T, self.error_list[-1]), self.d_activation_function(self.activation_input[i], self.slope_param)))
-            self.weights_error.append(np.dot(np.array([self.error_list[-1]]).T, np.array([self.data_layers[i]])))
-
-        if self.normalize_input:
-            self.weights_error[-1] = np.multiply(self.error_list[-1], self.data_layers[0])
+            self.error_list.append(
+                np.multiply(np.dot(self.network_weights[i + 1].T, 
+                self.error_list[-1]), 
+                self.d_activation_function(self.activation_input[i], 
+                self.slope_param)))
+            self.weights_error.append(
+                np.multiply(np.array([self.error_list[-1]]).T, 
+                np.array([self.data_layers[i]])))
 
         #Multiply by the correct adjustment and return
         self.error_list = list(reversed(self.error_list))
-        self.weights_error = list(reversed(self.weights_error))
-        return (self.weights_error, self.error_list)        
+        self.weights_error = list(reversed(self.weights_error))    #Weights error calculated outside in train function
+        square_sum_error = np.divide(
+            np.sum(np.square(self.data_layers[-1] - target_values)), 
+            len(target_values))
+        return (self.error_list, square_sum_error)        
 
     def adjustment(self, weights_adjust, bias_adjust):
+
         #Adjust the weights and biases to correct network.
         for i in range(len(self.network_weights)):
-            self.network_weights[i] = np.subtract(self.network_weights[i], weights_adjust[i])
-            self.network_bias[i] = np.subtract(self.network_bias[i], bias_adjust[i])
+            self.network_weights[i] = np.subtract(self.network_weights[i], 
+                weights_adjust[i])
+            self.network_bias[i] = np.subtract(self.network_bias[i], 
+                bias_adjust[i])
+
 
 class RNeuralNetwork(NeuralNetwork):
 
+
+
     def __init__(self, input_data):
+
         pass
 
+
+
 class HopfieldNetwork(RNeuralNetwork):
+
+
+
     pass
+
+
 
 class BoltzmannMachine(RNeuralNetwork):
+
+
+
     pass
+
+
 
 class LongShortTermMemory(RNeuralNetwork):
+
+
+
     pass
 
+
+
 class StochasticNetwork(RNeuralNetwork):
+
+
+
     pass
 
 ##--------------------------Activation Functions-------------------------------
 
+def linear(input_stimulus):
+
+    return input_stimulus
+
+def d_linear(input_stimulus, slope_param=1.0):
+
+    return slope_param
+
 def log_sigmoid(input_stimulus):
 
     #Returns a value from 0 to 1 and has a variable slope.
-    sig_val = np.divide(1.0, (1.0 + (np.exp(np.multiply(-1., input_stimulus)))))
+    sig_val = np.divide(1.0, (1.0 + (np.exp(
+                  np.multiply(-1., input_stimulus)))))
     return sig_val
 		
 def d_log_sigmoid(input_stimulus, slope_param=1.0):
+
     #Returns the derivative of the log_sigmoid activation function.
     #The derivative is used when determing the error in a neuron.
-    sig_derivative = np.divide(slope_param, np.multiply(2.0, np.cosh(input_stimulus)) + 2) 
+    sig_derivative = np.divide(slope_param, 
+                         np.multiply(2.0, np.cosh(input_stimulus)) + 2)
     return sig_derivative
 	
 def tan_hyperbolic(input_stimulus):
+
     # Returns a value from -1 to 1. Similar to log_sigmoid but 
     # offers negative values.
     hyp_value = np.tanh(input_stimulus)
     return hyp_value
 		
 def d_tan_hyperbolic(input_stimulus, slope_param=1.0):
+
     # Returns the derivative of the tan_hyperbolic fxn.
     d_hyp_value = slope_param * np.square((np.sech(input_stimulus)))
     return d_hype_value
-
-##-----------------------------------------------------------------------------
 
 ##----------------------------Cost Function Gradients--------------------------
 
 #Many found here: http://stats.stackexchange.com/questions/154879/a-list-of-cost-functions-used-in-neural-networks-alongside-applications
 
-def quad_cost_grad(output_vector, truth_vector):
-    return (output_vector - truth_vector)
+def quad_cost_grad(output_vector, target_vector):
 
-def cross_entropy_cost_grad(output_vector, truth_vector):
-    return np.divide((output_vector - truth_vector), ((1. - output_vector) * output_vector))
+    return (output_vector - target_vector)
 
-def exponential_cost_grad(output_vector, truth_vector, t_param=1.0):
+def cross_entropy_cost_grad(output_vector, target_vector):
+
+    return np.divide((output_vector - target_vector), 
+                     ((1. - output_vector) * output_vector))
+
+def exponential_cost_grad(output_vector, target_vector, t_param=1.0):
+
     #Not sure if this is right. Need to check.
-    exp_val = (1. / t_param) * np.sum((output_vector - truth_vector) ** 2.) 
+    exp_val = (1. / t_param) * np.sum((output_vector - target_vector) ** 2.)
     cost_val = t_param * np.exp(exp_val)
-    coeff_vector = (2./t_param) * (output_vector - truth_vector)
+    coeff_vector = (2./t_param) * (output_vector - target_vector)
     return coeff_vector * cost_val 
 
-def Hellinger_distance_grad(output_vector, truth_vector):
+def Hellinger_distance_grad(output_vector, target_vector):
+
     #Requires positive values
-    return (np.sqrt(output_vector) - np.sqrt(truth_vector)) \
+    return (np.sqrt(output_vector) - np.sqrt(target_vector)) \
            / (np.sqrt(2) * np.sqrt(output_vector))
 
-def Kullback_Leibler_divergence_grad(output_vector, truth_vector):
-    return -1 * np.divide(truth_vector, output_vector)
+def Kullback_Leibler_divergence_grad(output_vector, target_vector):
 
-def gen_Kullback_Leibler_divergence_grad(output_vector, truth_vector):
-    return -1 * (np.divide((truth_vector + output_vector), output_vector))
+    return -1 * np.divide(target_vector, output_vector)
 
-def Itakura_Saito_distance_grad(output_vector, truth_vector):
-    return np.divide((output_vector - truth_vector), (output_vector) ** 2.)
+def gen_Kullback_Leibler_divergence_grad(output_vector, target_vector):
 
-##-----------------------------------------------------------------------------
+    return -1 * (np.divide((target_vector + output_vector), output_vector))
+
+def Itakura_Saito_distance_grad(output_vector, target_vector):
+
+    return np.divide((output_vector - target_vector), (output_vector) ** 2.)
 
 ##----------------------------Optimization Algorithms--------------------------
 
-def backpropagation(err_vector, wghts_matrix, deriv_vector, lrn_rate=1.0):
+def backpropagation():
    pass 
 
 def backpropagation_intime():
@@ -305,6 +407,7 @@ def some_kind_of_hybrid():
 
 def create_network(inputFile):
 
+
     input_data = inputFile
 
     if (input_data["networkClass"] == "Feedforward"):
@@ -313,7 +416,3 @@ def create_network(inputFile):
         if(input_data["networkType"] == "MLPercep"):
             return MultiLayerPerceptron(input_data)
 
-
-def test_networks():
-    #Check accuracy and correct shape of networks.
-    pass
